5ff6b6b2fbfa42de1724e7357a0d34f2de3241d8
Gerardo Rico Botella <grico@wearemarketing.com>
2020-11-07 17:05:43 +0100

    Fix project configuration

---
+++ discourse/Data/Client/Http/Response/ErrorResponse.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorResponse.swift
+//  discourse
+//
+//  Created by Gerardo Rico Botella on 04/11/2020.
+//
+
+import Foundation
+
+struct ErrorResponse: Codable {
+    var errors: [String]
+}
